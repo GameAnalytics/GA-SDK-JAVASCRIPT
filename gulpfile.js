c037990e1da01da2059d6f97a652902ac75f7a1a
Martin Treacy-Schwartz <mts@gameanalytics.com>
2020-02-04 13:57:11 +0100

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -13,66 +13,6 @@ var replace = require('gulp-replace');
 var concat = require('gulp-concat');
 var insert = require('gulp-insert');
 
-gulp.task('mini', ['bundle_min_js', 'build_mini'], function() {
-    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.min.js'])
-        .pipe(concat('GameAnalytics.min.js'))
-        .pipe(uglify())
-        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('build_mini', function() {
-    var tsResult = tsProjectMini.src()
-        .pipe(replace('GALogger.debugEnabled = true', 'GALogger.debugEnabled = false'))
-        .pipe(replace('GALogger.d(', '//GALogger.d('))
-        .pipe(gulpif(argv.nologging, replace('GALogger.', '//GALogger.')))
-        .pipe(gulpif(argv.nologging, replace('//GALOGGER_START', '/*GALOGGER_START')))
-        .pipe(gulpif(argv.nologging, replace('//GALOGGER_END', '//GALOGGER_END*/')))
-        .pipe(gulpif(argv.nologging, replace('import GALogger = gameanalytics.logging.GALogger', '//import GALogger = gameanalytics.logging.GALogger')))
-        .pipe(tsProjectMini());
-
-    return tsResult.js
-        .pipe(gulp.dest('.'));
-});
-
-gulp.task('normal', ['bundle_min_js', 'build_normal'], function() {
-    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.js'])
-        .pipe(concat('GameAnalytics.js'))
-        .pipe(uglify())
-        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('unity', ['bundle_js', 'build_normal'], function() {
-    return gulp.src(['./vendor/bundle.js', './dist/GameAnalytics.js'])
-        .pipe(concat('GameAnalytics.jspre'))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('ga_node', ['bundle_js', 'build_normal'], function() {
-    return gulp.src(['./vendor/bundle.js', './dist/GameAnalytics.js'])
-        .pipe(concat('GameAnalytics.node.js'))
-        .pipe(insert.wrap("'use strict';\n", "module.exports = gameanalytics;"))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('build_normal', function() {
-    var tsResult = tsProject.src()
-        .pipe(replace('GALogger.debugEnabled = true', 'GALogger.debugEnabled = false'))
-        .pipe(replace('GALogger.d(', '//GALogger.d('))
-        .pipe(tsProject());
-
-    return tsResult.js
-        .pipe(gulp.dest('.'));
-});
-
-gulp.task('debug', ['bundle_min_js', 'build_debug'], function() {
-    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.debug.js'])
-        .pipe(concat('GameAnalytics.debug.js'))
-        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
-        .pipe(gulp.dest('./dist'));
-});
-
 gulp.task('build_debug', function() {
     var tsResult = tsProjectDebug.src()
         .pipe(sourcemaps.init())
@@ -116,4 +56,64 @@ gulp.task('test', function (done) {
     }, done).start();
 });
 
-gulp.task('default', ['debug', 'mini', 'unity', 'ga_node', 'normal', 'declaration']);
+gulp.task('build_mini', function() {
+    var tsResult = tsProjectMini.src()
+        .pipe(replace('GALogger.debugEnabled = true', 'GALogger.debugEnabled = false'))
+        .pipe(replace('GALogger.d(', '//GALogger.d('))
+        .pipe(gulpif(argv.nologging, replace('GALogger.', '//GALogger.')))
+        .pipe(gulpif(argv.nologging, replace('//GALOGGER_START', '/*GALOGGER_START')))
+        .pipe(gulpif(argv.nologging, replace('//GALOGGER_END', '//GALOGGER_END*/')))
+        .pipe(gulpif(argv.nologging, replace('import GALogger = gameanalytics.logging.GALogger', '//import GALogger = gameanalytics.logging.GALogger')))
+        .pipe(tsProjectMini());
+
+    return tsResult.js
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('build_normal', function() {
+    var tsResult = tsProject.src()
+        .pipe(replace('GALogger.debugEnabled = true', 'GALogger.debugEnabled = false'))
+        .pipe(replace('GALogger.d(', '//GALogger.d('))
+        .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('mini', gulp.series('bundle_min_js', 'build_mini'), function() {
+    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.min.js'])
+        .pipe(concat('GameAnalytics.min.js'))
+        .pipe(uglify())
+        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('normal', gulp.series('bundle_min_js', 'build_normal'), function() {
+    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.js'])
+        .pipe(concat('GameAnalytics.js'))
+        .pipe(uglify())
+        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('unity', gulp.series('bundle_js', 'build_normal'), function() {
+    return gulp.src(['./vendor/bundle.js', './dist/GameAnalytics.js'])
+        .pipe(concat('GameAnalytics.jspre'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('ga_node', gulp.series('bundle_js', 'build_normal'), function() {
+    return gulp.src(['./vendor/bundle.js', './dist/GameAnalytics.js'])
+        .pipe(concat('GameAnalytics.node.js'))
+        .pipe(insert.wrap("'use strict';\n", "module.exports = gameanalytics;"))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('debug', gulp.series('bundle_min_js', 'build_debug'), function() {
+    return gulp.src(['./vendor/bundle.min.js', './dist/GameAnalytics.debug.js'])
+        .pipe(concat('GameAnalytics.debug.js'))
+        .pipe(insert.wrap("(function(scope){\n", "\nscope.gameanalytics=gameanalytics;\nscope.GameAnalytics=GameAnalytics;\n})(this);\n"))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', gulp.series('debug', 'mini', 'unity', 'ga_node', 'normal', 'declaration'));
