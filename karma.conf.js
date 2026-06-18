1c932c20219b1e1928301bf66ef0b7216bfc61c0
Dorin <dorin@gameanalytics.com>
2026-06-18 16:25:07 +0300

    unit tests for CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,13 @@
 module.exports = function(config) {
+    const isCI = !!process.env.CI;
     config.set({
-        browsers: ['ChromeHeadless'],
+        browsers: [isCI ? 'ChromeHeadlessCI' : 'ChromeHeadless'],
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-setuid-sandbox'],
+            },
+        },
         frameworks: ['jasmine'],
         files: [
             'dist/*.js',
